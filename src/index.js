8151f160342044660daa48d42b351e79c223cc21
leandro <leandro.vicente98@outlook.com>
2020-02-23 15:51:44 -0300

    SetState

--- src/index.js
+++ src/index.js
@@ -3,35 +3,41 @@ import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+  static PropTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
   render() {
     return (
-      <a href="" onClick={this.props.onClick}>
+      <button href="" onClick={this.props.onClick}>
         {this.props.children}
-      </a>
+      </button>
     );
   }
 }
-Button.defaultProps = {
-  children: "Salvar"
-};
-Button.PropTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
-};
 class App extends Component {
-  handleClick() {
-    alert("Botão Clickado");
-  }
+  state = {
+    counter: 0
+  };
+  handleClick = () => {
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+  };
+
   render() {
     return (
       <Fragment>
         <h1>Hello</h1>
-        <Button
-          onClick={() => {
-            alert("Button 1");
-          }}
-        />
-        <Button onClick={this.handleClick}>Enviar</Button>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
